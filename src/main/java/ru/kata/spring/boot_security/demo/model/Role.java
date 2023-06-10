bc4091d4b301f63f7c1b41464dce180d33cd377a
OlegZ <beyondswamps@gmail.com>
2023-06-10 21:26:26 +0300

    added UserDetails and GrantedAuthority interfaces

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -1,6 +1,6 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import java.util.Objects;
@@ -8,7 +8,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -56,5 +56,10 @@ public class Role {
     @Override
     public int hashCode() {
         return Objects.hash(id, name, users);
+    }
+
+    @Override
+    public String getAuthority() {
+        return name;
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -2,19 +2,25 @@ package ru.kata.spring.boot_security.demo.model;
 
 import javax.persistence.*;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
 @Component
-public class User {
+public class User implements UserDetails {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="username")
+    private String username;
 
     @Column(name = "name")
     private String name;
@@ -28,6 +34,9 @@ public class User {
     @Column(name = "roles")
     @ManyToMany(fetch = FetchType.LAZY)
     private Set<Role> roles;
+
+    @Column(name="password")
+    private String password;
 
     public User(String name, String surname, Integer age) {
         this.name = name;
@@ -85,6 +94,14 @@ public class User {
         this.roles = roles;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -106,5 +123,40 @@ public class User {
                 ", surname='" + surname + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
