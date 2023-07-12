6033b39e02090316909520ceec391e5c53634011
OlegZ <beyondswamps@gmail.com>
2023-07-12 10:15:46 +0300

    new user form is done. Started modal part of edit user

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -53,6 +54,8 @@ public class AdminController {
     @GetMapping("/")
     public String listUsers(Model model, User user) {
         model.addAttribute("users", userService.getUsers());
+        model.addAttribute("user", new User());
+        model.addAttribute("allRoles", roleService.getAllRoles());
         return "users";
     }
 
@@ -77,6 +80,6 @@ public class AdminController {
     @GetMapping(value = "/delete")
     public String deleteUser(@RequestParam Long id) {
         userService.deleteUser(id);
-        return "redirect:/admin/all";
+        return "redirect:/admin/";
     }
 }
